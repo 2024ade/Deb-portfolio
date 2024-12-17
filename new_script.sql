567b3b82aabbc53bb4bb4d6af59374476143e318
2024ade <162434753+2024ade@users.noreply.github.com>
2024-12-16 22:42:14 -1200

    Add files via upload

---
+++ new_script.sql
@@ -0,0 +1,14 @@
+---Create Database 
+USE  sql_class_June;
+
+---Retrieve all data from train_data
+SELECT
+     *
+FROM
+    customer_info;
+
+---Retrieve customer_name from the customer_info table
+SELECT
+     customer_name
+FROM 
+     customer_info;
